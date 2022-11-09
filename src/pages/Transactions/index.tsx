cfa59cd66975698208c3e796ddffaac7b69f657e
renanlimaleite <renan.lima.softwares@gmail.com>
2022-11-09 08:50:24 -0300

    Add: table transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import * as S from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <S.TransactionsContainer>
+        <S.TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <S.PriceHighlight variant="income">
+                  R$ 12.000,00
+                </S.PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <S.PriceHighlight variant="outcome">
+                  - R$ 59.00
+                </S.PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2023</td>
+            </tr>
+          </tbody>
+        </S.TransactionsTable>
+      </S.TransactionsContainer>
     </div>
   );
 }
