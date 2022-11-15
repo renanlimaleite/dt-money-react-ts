8ebdf1c88f827b650f7d4a2be81faed3f62ebbde
renanlimaleite <renan.lima.softwares@gmail.com>
2022-11-15 15:29:46 -0300

    transaction modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import * as S from "./styles";
+import * as Dialog from '@radix-ui/react-dialog'
 
 import logoImg from "../../assets/logo.svg";
 
@@ -7,7 +8,19 @@ export function Header() {
     <S.HeaderContainer>
       <S.HeaderContent>
         <img src={logoImg} alt="DT Money Logo" />
-        <S.NewTransactionButton>Nova transação</S.NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <S.NewTransactionButton>Nova transação</S.NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </S.HeaderContent>
     </S.HeaderContainer>
   );
